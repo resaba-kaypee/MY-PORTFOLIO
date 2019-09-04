6094daba80e112719b2e4d5fd8ad3c148b3c41f9
kaypee <resaba.kaypee@gmail.com>
2019-09-05 04:13:58 +0700

    added typewriting effect

--- js/script.js
+++ js/script.js
@@ -1,3 +1,76 @@
+document.addEventListener('DOMContentLoaded', getElems);
+
+/* SCROLL EFFECT
+-----------------------------------------------------*/
+scrollSpy('#menu');
+
+/* HOME - TYPE EFFECT
+-----------------------------------------------------*/
+
+class TypeWriter {
+  constructor(txtElem, words, wait = 3000){
+    this.txtElem = txtElem;
+    this.words = words;
+    this.txt = '';
+    this.wIndx = 0;
+    this.wait = parseInt(wait, 10);
+    this.type();
+    this.isDeleting = false;
+  }
+
+  type(){
+
+    const current = this.wIndx % this.words.length;
+    const fullTxt = this.words[current];
+    let typeSpeed = 300;
+
+    // console.log(fullTxt)
+    if(this.isDeleting){
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.txtElem.innerHTML = `<span class="txt-cursor">${this.txt}</span>`
+
+    if(this.isDeleting) {
+      typeSpeed /= 2;
+    }
+
+    if(!this.isDeleting && this.txt === fullTxt){
+      typeSpeed = this.wait;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === ''){
+      this.isDeleting = false;
+      this.wIndx++;
+
+      typeSpeed = 500;
+    }
+    setTimeout(() => this.type(), typeSpeed);
+  }
+}
+
+function getElems(){
+  const txtElem = document.querySelector('.text');
+  const words = JSON.parse(txtElem.getAttribute('data-words'));
+  const wait = txtElem.getAttribute('data-wait');
+  return new TypeWriter(txtElem, words, wait);
+}
+
+// getElems();
+
+
+
+
+
+
+
+
+
+
+
+
+
 // const list = document.querySelectorAll('a');
 
 // list.forEach(link => {
@@ -11,9 +84,3 @@
 //   }
 //  e.target.className = "active";
 // }
-
-/* SCROLL EFFECT
------------------------------------------------------*/
-window.onload = function () {
-  scrollSpy('#menu');
-}
