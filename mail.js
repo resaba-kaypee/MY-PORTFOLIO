32ddee3a6901e5bd9a5f3c39f42b8f5fc97876a7
kaypee <resaba.kaypee@gmail.com>
2019-09-18 19:44:48 +0700

    updated mail security

--- mail.js
+++ mail.js
@@ -1,21 +1,50 @@
 require('dotenv').config();
+const nodemailer = require('nodemailer');
+const mg = require('nodemailer-mailgun-transport');
+
 const api_key = process.env.API_KEY;
 const DOMAIN = process.env.MY_DOMAIN;
-const mailgun = require('mailgun-js')({ apiKey: api_key, domain: DOMAIN });
+// const mailgun = require('mailgun-js')({ apiKey: api_key, domain: DOMAIN });
+
+// function sendMail(name, email, subject, message, cb){
+
+//   const data = {
+//     from: `${name} ` + email,
+//     to: process.env.MY_EMAIL,
+//     subject: subject,
+//     text: message,
+//   };
+  
+//   mailgun.messages().send(data, function (err, body) {
+//     if(err) return cb(err, null);
+//     else return cb(null, body);
+//   });
+// }
+
+const auth = {
+  auth: {
+    api_key: api_key,
+    domain: DOMAIN
+  }
+}
+
+const nodemailerMailgun = nodemailer.createTransport(mg(auth));
 
 function sendMail(name, email, subject, message, cb){
-
-  const data = {
+  nodemailerMailgun.sendMail({
     from: `${name} ` + email,
     to: process.env.MY_EMAIL,
     subject: subject,
     text: message,
-  };
-  
-  mailgun.messages().send(data, function (err, body) {
-    if(err) return cb(err, null);
-    else return cb(null, body);
+  }, (err, info) => {
+    if (err) {
+      return cb(err, null);
+    }
+    else {
+      return cb(null, info);
+    }
   });
 }
+
 
 module.exports = sendMail;
